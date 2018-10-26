74d6094c2009ed51e3c357fb92610c8811cc8073
Zdenek Preisler <z.preisler@gmail.com>
2018-10-26 15:58:49 -0700

    model

---
+++ model/data_pipeline.py
@@ -0,0 +1,41 @@
+def data_pipeline(inputs=None,outputs=None,batch=2048,n=1024):
+        import tensorflow as tf
+        from numpy import linspace,zeros,array
+        """
+        train dataset
+        """
+        length=len(inputs)
+        if batch>length:
+            batch=length
+
+        dataset=tf.data.Dataset.from_tensor_slices( 
+                {'inputs': inputs,
+                    'outputs': outputs}
+                )
+        train_dataset=dataset.repeat().shuffle(length).batch(batch)
+
+        iterator=tf.data.Iterator.from_structure(
+                train_dataset.output_types,
+                train_dataset.output_shapes)
+        next_element=iterator.get_next()
+        init_train_op=iterator.make_initializer(train_dataset)
+
+        """
+        eval dataset
+        """
+
+        x=[]
+        for k in inputs.transpose():
+            x+=[linspace(k.min(),k.max(),n)]
+        x=array(x).transpose()
+        z=zeros((n,outputs.shape[-1]))
+
+        dataset=tf.data.Dataset.from_tensor_slices( 
+                {'inputs': x,
+                    'outputs': z}
+                )
+        eval_dataset=dataset.batch(n)
+
+        init_eval_op=iterator.make_initializer(eval_dataset)
+
+        return next_element,init_train_op,init_eval_op
--- model/model.py
+++ model/model.py
@@ -1,8 +1,10 @@
 class network(object):
     def __init__(self,inputs,outputs,name='network'):
         import tensorflow as tf
-        print("Initialize %s"%name)
+        print('Initialize %s'%name)
+
         self.rate=tf.placeholder(tf.float64)
+
         self.inputs=inputs
         self.outputs=outputs
 
@@ -12,7 +14,7 @@ class network(object):
         self.define_optimizer()
         self.define_training()
 
-    def build_graph(self,inputs,output_dim,name='network'):
+    def build_graph(self,inputs,output_dim,name):
         import tensorflow as tf
         with tf.variable_scope(name):
             self.dense_1=tf.layers.dense(inputs=inputs,
@@ -49,6 +51,7 @@ class network(object):
 class flow(object):
     def __init__(self,name='flow'):
         import tensorflow as tf
+        from .data_pipeline import data_pipeline
 
         self.c=data_feeder(files='eos/fluid*.conf',
                 add_data=['.en','.rho'])
@@ -59,7 +62,7 @@ class flow(object):
         inputs=self.data[:,:2] #epsilon,pressure
         outputs=self.data[:,2:] #en,rho
 
-        next_element,self.init_train_op,self.init_eval_op=self.data_pipeline(inputs=inputs,
+        next_element,self.init_train_op,self.init_eval_op=data_pipeline(inputs=inputs,
                 outputs=outputs)
 
         """
@@ -67,48 +70,48 @@ class flow(object):
         """
         self.nn=network(next_element['inputs'],next_element['outputs'],name="Kagami")
 
-    def data_pipeline(self,inputs=None,outputs=None,batch=2048,n=1024):
-        import tensorflow as tf
-        from numpy import linspace,zeros,array
-        """
-        train dataset
-        """
-        length=len(inputs)
-        if batch>length:
-            batch=length
-
-        dataset=tf.data.Dataset.from_tensor_slices( 
-                {'inputs': inputs,
-                    'outputs': outputs}
-                )
-        train_dataset=dataset.repeat().shuffle(length).batch(batch)
-
-        iterator=tf.data.Iterator.from_structure(
-                train_dataset.output_types,
-                train_dataset.output_shapes)
-        next_element=iterator.get_next()
-        init_train_op=iterator.make_initializer(train_dataset)
-
-        """
-        eval dataset
-        """
-
-        x=[]
-        for k in inputs.transpose():
-            x+=[linspace(k.min(),k.max(),n)]
-        x=array(x).transpose()
-        z=zeros((n,outputs.shape[-1]))
-
-        dataset=tf.data.Dataset.from_tensor_slices( 
-                {'inputs': x,
-                    'outputs': z}
-                )
-        eval_dataset=dataset.batch(n)
-
-        init_eval_op=iterator.make_initializer(eval_dataset)
-
-        return next_element,init_train_op,init_eval_op
-
+#    def data_pipeline(self,inputs=None,outputs=None,batch=2048,n=1024):
+#        import tensorflow as tf
+#        from numpy import linspace,zeros,array
+#        """
+#        train dataset
+#        """
+#        length=len(inputs)
+#        if batch>length:
+#            batch=length
+#
+#        dataset=tf.data.Dataset.from_tensor_slices( 
+#                {'inputs': inputs,
+#                    'outputs': outputs}
+#                )
+#        train_dataset=dataset.repeat().shuffle(length).batch(batch)
+#
+#        iterator=tf.data.Iterator.from_structure(
+#                train_dataset.output_types,
+#                train_dataset.output_shapes)
+#        next_element=iterator.get_next()
+#        init_train_op=iterator.make_initializer(train_dataset)
+#
+#        """
+#        eval dataset
+#        """
+#
+#        x=[]
+#        for k in inputs.transpose():
+#            x+=[linspace(k.min(),k.max(),n)]
+#        x=array(x).transpose()
+#        z=zeros((n,outputs.shape[-1]))
+#
+#        dataset=tf.data.Dataset.from_tensor_slices( 
+#                {'inputs': x,
+#                    'outputs': z}
+#                )
+#        eval_dataset=dataset.batch(n)
+#
+#        init_eval_op=iterator.make_initializer(eval_dataset)
+#
+#        return next_element,init_train_op,init_eval_op
+#
 from myutils import configuration,data
 class data_feeder(configuration):
     def __init__(self,files,add_data=[],delimiter=[':']):
